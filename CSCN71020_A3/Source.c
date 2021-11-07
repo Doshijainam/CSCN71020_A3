3df8acfe00cf3667a73e9a7140b6b76267d2dd1d
Jainam Yogesh Doshi <Jdoshi3117@conestogac.on.ca>
2021-11-07 17:21:03 -0500

    Unit tests updated

--- CSCN71020_A3/Source.c
+++ CSCN71020_A3/Source.c
@@ -9,36 +9,44 @@ char* Rock_Paper_Scissor(char p1[LENGTH], char p2[LENGTH]);
 
 char* Rock_Paper_Scissor(char p1[LENGTH], char p2[LENGTH])
 {
-    char* result[LENGTH];
+    char* result;
+    result = (char*)malloc(10 * sizeof(char));
+    
     if (strcmp(p1, "Rock") == 0 && strcmp(p2, "Rock") == 0 || strcmp(p1, "Paper") == 0 && strcmp(p2, "Paper") == 0 || strcmp(p1, "Scissor") == 0 && strcmp(p2, "Scissor") == 0)
     {
         strcpy_s(result, 10, "Draw");
     }
+    
 
-    if (strcmp(p1, "Rock") == 0 && strcmp(p2, "Scissor") == 0 || strcmp(p1, "Paper") == 0 && strcmp(p2, "Rock") == 0 || strcmp(p1, "Scissor") == 0 && strcmp(p2, "Paper") == 0)
+    else if (strcmp(p1, "Rock") == 0 && strcmp(p2, "Scissor") == 0 || strcmp(p1, "Paper") == 0 && strcmp(p2, "Rock") == 0 || strcmp(p1, "Scissor") == 0 && strcmp(p2, "Paper") == 0)
     {
         strcpy_s(result, 10, "Player1");
     }
+    
 
-    if (strcmp(p1, "Rock") == 0 && strcmp(p2, "Paper") == 0 || strcmp(p1, "Paper") == 0 && strcmp(p2, "Scissor") == 0 || strcmp(p1, "Scissor") == 0 && strcmp(p2, "Rock") == 0)
+    else if (strcmp(p1, "Rock") == 0 && strcmp(p2, "Paper") == 0 || strcmp(p1, "Paper") == 0 && strcmp(p2, "Scissor") == 0 || strcmp(p1, "Scissor") == 0 && strcmp(p2, "Rock") == 0)
     {
         strcpy_s(result, 10, "Player2");
     }
-    if (strcmp(p1, "Rock") != 0 && strcmp(p1, "Paper") != 0 && strcmp(p1, "Scissor") != 0 && strcmp(p2, "Rock") != 0 && strcmp(p2, "Paper") != 0 && strcmp(p2, "Scissor") != 0)
+   
+    else
     {
-        strcpy_s(result, 10, "Invalid");
+        strcpy_s(result, 10, "Invalid!");
     }
+   
     return (char*)result;
 }
 
 int main()
 {
     char p1[10], p2[10];
+    char* answer;
     printf("Rock Paper Scissor Simulator\n\nPlease choose from the following\n\nRock\nPaper\nScissor\n\n");
     printf("Player 1:");
     scanf_s("%s", p1, 10);
     printf("\nPayer 2:");
     scanf_s("%s", p2, 10);
-    printf("\n%s", Rock_Paper_Scissor(p1, p2));
-
+    answer = Rock_Paper_Scissor(p1, p2);
+    printf("\n%s", answer);
+   
 }
--- UnitTestDirectory/UnitTestDirectory.cpp
+++ UnitTestDirectory/UnitTestDirectory.cpp
@@ -10,34 +10,181 @@ namespace UnitTestDirectory
 	TEST_CLASS(UnitTestDirectory)
 	{
 	public:
-		
+
 		TEST_METHOD(case_Sensitivity_Test1)
 		{
-			char p1[LENGTH] = "paper";
-			char p2[LENGTH] = "scissor";
+			char p1[LENGTH] = "Rock";
+			char p2[LENGTH] = "Paper";
 			char expected_result[LENGTH] = "Player2";
-			char* result;
-			result = Rock_Paper_Scissor(p1, p2);
-			Assert::AreEqual(expected_result, result);
+			char* final_solution;
+			final_solution = Rock_Paper_Scissor(p1, p2);
+			Assert::AreEqual(expected_result, final_solution);
 		}
 		TEST_METHOD(case_Sensitivity_Test2)
 		{
 			char p1[LENGTH] = "rock";
-			char p2[LENGTH] = "scissor";
-			char expected_result[LENGTH] = "Player1";
-			char* result;
-			result = Rock_Paper_Scissor(p1, p2);
-			Assert::AreEqual(expected_result, result);
+			char p2[LENGTH] = "paper";
+			char expected_result[LENGTH] = "Player2";
+			char* final_solution;
+			final_solution = Rock_Paper_Scissor(p1, p2);
+			Assert::AreEqual(expected_result, final_solution);
 		}
 		TEST_METHOD(case_Sensitivity_Test3)
+		{
+			char p1[LENGTH] = "Rock";
+			char p2[LENGTH] = "Scissor";
+			char expected_result[LENGTH] = "Player1";
+			char* final_solution;
+			final_solution = Rock_Paper_Scissor(p1, p2);
+			Assert::AreEqual(expected_result, final_solution);
+		}
+		TEST_METHOD(case_Sensitivity_Test4)
+		{
+			char p1[LENGTH] = "rock";
+			char p2[LENGTH] = "scissor";
+			char expected_result[LENGTH] = "Player1";
+			char* final_solution;
+			final_solution = Rock_Paper_Scissor(p1, p2);
+			Assert::AreEqual(expected_result, final_solution);
+		}
+		TEST_METHOD(case_Sensitivity_Test5)
+		{
+			char p1[LENGTH] = "Rock";
+			char p2[LENGTH] = "Rock";
+			char expected_result[LENGTH] = "Draw";
+			char* final_solution;
+			final_solution = Rock_Paper_Scissor(p1, p2);
+			Assert::AreEqual(expected_result, final_solution);
+		}
+		TEST_METHOD(case_Sensitivity_Test6)
+		{
+			char p1[LENGTH] = "rock";
+			char p2[LENGTH] = "rock";
+			char expected_result[LENGTH] = "Draw";
+			char* final_solution;
+			final_solution = Rock_Paper_Scissor(p1, p2);
+			Assert::AreEqual(expected_result, final_solution);
+		}
+		TEST_METHOD(case_Sensitivity_Test7)
+		{
+			char p1[LENGTH] = "Paper";
+			char p2[LENGTH] = "Rock";
+			char expected_result[LENGTH] = "Player1";
+			char* final_solution;
+			final_solution = Rock_Paper_Scissor(p1, p2);
+			Assert::AreEqual(expected_result, final_solution);
+		}
+		TEST_METHOD(case_Sensitivity_Test8)
+		{
+			char p1[LENGTH] = "paper";
+			char p2[LENGTH] = "rock";
+			char expected_result[LENGTH] = "Player1";
+			char* final_solution;
+			final_solution = Rock_Paper_Scissor(p1, p2);
+			Assert::AreEqual(expected_result, final_solution);
+		}
+		TEST_METHOD(case_Sensitivity_Test9)
+		{
+			char p1[LENGTH] = "Paper";
+			char p2[LENGTH] = "Paper";
+			char expected_result[LENGTH] = "Draw";
+			char* final_solution;
+			final_solution = Rock_Paper_Scissor(p1, p2);
+			Assert::AreEqual(expected_result, final_solution);
+		}
+		TEST_METHOD(case_Sensitivity_Test10)
+		{
+			char p1[LENGTH] = "paper";
+			char p2[LENGTH] = "paper";
+			char expected_result[LENGTH] = "Draw";
+			char* final_solution;
+			final_solution = Rock_Paper_Scissor(p1, p2);
+			Assert::AreEqual(expected_result, final_solution);
+		}
+
+		TEST_METHOD(case_Sensitivity_Test11)
+		{
+			char p1[LENGTH] = "Paper";
+			char p2[LENGTH] = "Scissor";
+			char expected_result[LENGTH] = "Player2";
+			char* final_solution;
+			final_solution = Rock_Paper_Scissor(p1, p2);
+			Assert::AreEqual(expected_result, final_solution);
+		}
+		TEST_METHOD(case_Sensitivity_Test12)
+		{
+			char p1[LENGTH] = "paper";
+			char p2[LENGTH] = "scissor";
+			char expected_result[LENGTH] = "Player2";
+			char* final_solution;
+			final_solution = Rock_Paper_Scissor(p1, p2);
+			Assert::AreEqual(expected_result, final_solution);
+		}
+		TEST_METHOD(case_Sensitivity_Test13)
+		{
+			char p1[LENGTH] = "Scissor";
+			char p2[LENGTH] = "Rock";
+			char expected_result[LENGTH] = "Player2";
+			char* final_solution;
+			final_solution = Rock_Paper_Scissor(p1, p2);
+			Assert::AreEqual(expected_result, final_solution);
+		}
+		TEST_METHOD(case_Sensitivity_Test14)
+		{
+			char p1[LENGTH] = "scissor";
+			char p2[LENGTH] = "rock";
+			char expected_result[LENGTH] = "Player2";
+			char* final_solution;
+			final_solution = Rock_Paper_Scissor(p1, p2);
+			Assert::AreEqual(expected_result, final_solution);
+		}
+		TEST_METHOD(case_Sensitivity_Test15)
+		{
+			char p1[LENGTH] = "Scissor";
+			char p2[LENGTH] = "Paper";
+			char expected_result[LENGTH] = "Player1";
+			char* final_solution;
+			final_solution = Rock_Paper_Scissor(p1, p2);
+			Assert::AreEqual(expected_result, final_solution);
+		}
+		TEST_METHOD(case_Sensitivity_Test16)
+		{
+			char p1[LENGTH] = "scissor";
+			char p2[LENGTH] = "paper";
+			char expected_result[LENGTH] = "Player1";
+			char* final_solution;
+			final_solution = Rock_Paper_Scissor(p1, p2);
+			Assert::AreEqual(expected_result, final_solution);
+		}
+		TEST_METHOD(case_Sensitivity_Test17)
+		{
+			char p1[LENGTH] = "Scissor";
+			char p2[LENGTH] = "Scissor";
+			char expected_result[LENGTH] = "Draw";
+			char* final_solution;
+			final_solution = Rock_Paper_Scissor(p1, p2);
+			Assert::AreEqual(expected_result, final_solution);
+		}
+		TEST_METHOD(case_Sensitivity_Test18)
 		{
 			char p1[LENGTH] = "scissor";
 			char p2[LENGTH] = "scissor";
 			char expected_result[LENGTH] = "Draw";
-			char* result;
-			result = Rock_Paper_Scissor(p1, p2);
-			Assert::AreEqual(expected_result, result);
+			char* final_solution;
+			final_solution = Rock_Paper_Scissor(p1, p2);
+			Assert::AreEqual(expected_result, final_solution);
 		}
-	
+		TEST_METHOD(case_Sensitivity_Test19)
+		{
+			char p1[LENGTH] = "Hello";
+			char p2[LENGTH] = "World";
+			char expected_result[LENGTH] = "Invalid!";
+			char* final_solution;
+			final_solution = Rock_Paper_Scissor(p1, p2);
+			Assert::AreEqual(expected_result, final_solution);
+		}
+
+		
+
 	};
 }
